3ea59168b568eafdb255a563d8a64772339bc419
Abir Pal <30786216+abir4u@users.noreply.github.com>
2018-10-01 05:16:04 +1300

    Update ConstantsFile.java

--- Skyscanner/src/main/java/SkyscannerConstants/ConstantsFile.java
+++ Skyscanner/src/main/java/SkyscannerConstants/ConstantsFile.java
@@ -7,6 +7,6 @@ public class ConstantsFile {
     public static final String errorMessage = "Sorry, no direct results found – now showing results with stops.";
     public static final String searchPageTitle = "Cheap Flights, Hotels, Car Hire Worldwide - Skyscanner NZ";
     public static final String searchPageURL = "https://www.skyscanner.co.nz/";
-    public static final String resultsPageURLAKL2SIN = "https://www.skyscanner.co.nz/transport/flights/akln/sin/181015/181020/?adults=1&children=0&adultsv2=1&childrenv2=&infants=0&cabinclass=economy&rtn=1&preferdirects=true&outboundaltsenabled=true&inboundaltsenabled=true&ref=home#results";
-    public static final String resultsPageURL4CHC2PAT = "https://www.skyscanner.co.nz/transport/flights/chc/pat/181015/181020/?adults=1&children=0&adultsv2=1&childrenv2=&infants=0&cabinclass=economy&rtn=1&preferdirects=true&outboundaltsenabled=true&inboundaltsenabled=true&ref=home#results";
+    public static final String resultsPageURLAKL2SIN = "https://www.skyscanner.co.nz/transport/flights/akln/sin/181015/181020/?adults=1&children=0&adultsv2=1&childrenv2=&infants=0&cabinclass=economy&rtn=1&preferdirects=true&outboundaltsenabled=true&inboundaltsenabled=true&ref=home";
+    public static final String resultsPageURL4CHC2PAT = "https://www.skyscanner.co.nz/transport/flights/chc/pat/181015/181020/?adults=1&children=0&adultsv2=1&childrenv2=&infants=0&cabinclass=economy&rtn=1&preferdirects=true&outboundaltsenabled=true&inboundaltsenabled=true&ref=home";
 }
